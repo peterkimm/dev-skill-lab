1aa3bd6341f8c189769b6ef3907d6ac274f3ac3e
Peter <peterkimm35@gmail.com>
2022-05-08 18:34:09 -0700

    final

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
     console.log("ready");
 });
 
-$('.skills button').on('click', function () {
+$('.skills').on("click", "button", function() {
     const mySkills = $('input').val();
     const $newSkills = $(`<li><button>X</button>${mySkills}</li>`);
-    $('ul.listOfSkills').append($newSkills);
     $('input').val('');
+    $('ul.listOfSkills').append($newSkills);
 });
 
-$('ul').on('click', 'li button', function () {
+$('ul').on('click', 'li button', function() {
     $(this)
         .closest('li')
         .fadeOut(1000, function () {

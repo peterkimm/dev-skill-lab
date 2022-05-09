c067e3683d0dfbb0ea4d07c4856982daa1b4a521
Peter <peterkimm35@gmail.com>
2022-05-08 18:04:25 -0700

    finished

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
-
-$('#addSkills').on('click', function() {
-//    ...input 
+$(document).ready(function() {
+    console.log("ready");
 });
 
-$('.listOfSkills').on('click', function() {
-//    ...remove
+$('.skills button').on('click', function () {
+    const mySkills = $('input').val();
+    const $newSkills = $(`<li><button>X</button>${mySkills}</li>`);
+    $('ul.listOfSkills').append($newSkills);
+    $('input').val('');
+});
+
+$('ul').on('click', 'li button', function () {
+    $(this)
+        .closest('li')
+        .fadeOut(1000, function () {
+            $(this).remove()
+        })
 });
